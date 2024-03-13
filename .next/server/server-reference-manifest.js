df0f4249ff68a2b2456d647b56ea9afe264a4d18
masterBekshoh <bekshohdeveloper@gmail.com>
2024-03-13 08:08:58 +0500

    Full Project Files

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VHntbGyWhMlocDpdHVK4eN5+ZHKwscXWwRuvdvwjxCA=\"\n}"
---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'us-east-1-shared-usea1-02.graphassets.com',
+			},
+		],
+	},
+}
+
+export default nextConfig
